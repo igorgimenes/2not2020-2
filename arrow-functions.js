1665a3c7d8c96308130f541e1245d22cbf98230f
igorgimenes <igor_gimenes13@hotmail.com>
2020-08-28 23:35:05 +0000

    28/08 - atividade revisao

--- arrow-functions.js
+++ arrow-functions.js
@@ -35,3 +35,44 @@ let aleatorio2 = () => Math.floor(Math.random() * 1000)
 
 console.log('---------')
 console.log(aleatorio2())
+
+// Situação alternativa 2: função com 1+ argumento
+
+// Função tradicioanl com 1+ argumentos
+let imc = function(peso, altura) {
+    return peso / (altura ** 2)
+}
+
+// Arrow function com 1+ argumento
+// Os parenteses dos argumentos são obrigatórios quando o nº de argumentos é > 1
+let imc2 = (peso, altura) => peso / (altura ** 2)
+
+console.log('---------')
+console.log(imc(87, 1.78))
+console.log(imc2(87, 1.78))
+
+// CONCLUSÂO: os parenteses só podem ser omitidos da lista de argumentos
+// de uma arrow function quando há UM e APENAS UM argumento
+
+// Situação alternativa 3: funções com mais de uma linha no corpo
+
+// Função tradicional
+let fatorial = function(n) {
+    let res = 1
+    for(let i = n; i >= 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+// Arrow function com corpo maior que uma linha
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i > 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+console.log(fatorial(5))
+console.log(fatorial2(5))
---
+++ ativ01-revisao-javascript.js
@@ -0,0 +1,147 @@
+/*
+
+   DADOS PARA O EXERCÍCIO
+
+   nome;sigla;area;populacao
+   Sergipe;SE;21915.08;2278308
+   Alagoas;AL;27848.14;3322820
+   Rio Grande do Norte;RN;52811.05;3479010
+   Paraíba;PB;56469.78;3996496
+   Pernambuco;PE;98148.32;9496294
+   Ceará;CE;148920.47;9075649
+   Piauí;PI;251577.74;3264531
+   Maranhão;MA;331937.45;7035055
+   Bahia;BA;564733.18;14812617
+
+*/
+
+let estadosNe = []
+
+let estado1 = {
+   nome: 'Sergipe',
+   sigla: 'SE',
+   area: 21915.08,
+   populacao: 2278308
+}
+
+estadosNe.push(estado1)
+
+/*
+   1) Crie objetos para os demais Estados, segundo o modelo acima, e acrescente-os
+      ao vetor estadosNe usando push().
+*/
+// Alagoas;AL;27848.14;3322820
+let estado2 = {
+   nome: 'Alagoas',
+   sigla: 'AL',
+   area: 27848.14,
+   populacao: 3322820
+}
+// Rio Grande do Norte;RN;52811.05;3479010
+let estado3 = {
+   nome: 'Rio Grande do Norte',
+   sigla: 'RN',
+   area: 52811.05,
+   populacao: 3479010
+}
+// Paraíba;PB;56469.78;3996496
+let estado4 = {
+   nome: 'Paraíba',
+   sigla: 'PB',
+   area: 56469.78,
+   populacao: 3996496
+}
+// Pernambuco;PE;98148.32;9496294
+let estado5 = {
+   nome: 'Pernambuco',
+   sigla: 'PE',
+   area: 98148.32,
+   populacao: 9496294
+}
+// Ceará;CE;148920.47;9075649
+let estado6 = {
+   nome: 'Ceará',
+   sigla: 'CE',
+   area: 148920.47,
+   populacao: 9075649
+}
+//Piauí;PI;251577.74;3264531
+let estado7 = {
+   nome: 'Piauí',
+   sigla: 'PI',
+   area: 251577.74,
+   populacao: 3264531
+}
+// Maranhão;MA;331937.45;7035055
+let estado8 = {
+   nome: 'Maranhão',
+   sigla: 'MA',
+   area: 331937.45,
+   populacao: 7035055
+}
+// Bahia;BA;564733.18;14812617
+let estado9 = {
+   nome: 'Bahia',
+   sigla: 'BA',
+   area: 564733.18,
+   populacao: 14812617
+}
+
+estadosNe.push(estado2, estado3, estado4, estado5, estado6, estado7, estado8, estado9)
+console.log(estadosNe)
+
+/*
+   2) Escreva uma arrow function que receba dois parâmetros, área e população, e
+      calcule a densidade demográfica retornando o resultado da divisão da
+      segunda pela primeira. 
+*/
+
+let densiDemo = (pop, area) => pop / area
+
+//console.log(densiDemo(1000, 10))
+console.log('----------------------------------------------')
+
+/*
+   3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
+      chame a arrow function criada em 2) e efetue o cálculo correspondente.
+      Com o resultado do cálculo, crie uma propriedade chamada 'densidade
+      demográfica' para cada objeto representando um Estado.
+
+      Durante este mesmo loop, elimine a propriedade 'sigla' dos objetos.
+
+*/
+
+for(let i = 0; i < estadosNe.length; i++) {
+    let densidade = densiDemo(estadosNe[i].populacao, estadosNe[i].area)
+    estadosNe[i]['densidade demográfica'] = densidade
+    delete estadosNe[i].sigla   
+}
+
+console.log(estadosNe) 
+
+/* 4) Escreva uma arrow function que receba um objeto. Na função, use for..in
+      para extrair as propriedades e seus valores e exibi-los com console.log().
+
+*/
+
+let estado = n => {
+    for(let atrib in estado1) { // -> errado isso aqui mudar tetar com obj separado
+        console.log(`Atributo: ${atrib}; valor: ${estado1[atrib]}`)
+    }
+}
+
+/* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
+      invoque a função escrita em 4) para exibi-lo.
+
+/*
+   6)
+      a) Declare um vetor vazio.
+      b) Insira no vetor criado no item a) apenas o nome de cada Estado, conforme
+         os dados no topo deste arquivo, um de cada vez. Faça as inserções de 
+         modo que o vetor final esteja em ordem alfabética. No caso, o primeiro 
+         Estado (Sergipe), pode ser inserido com push(), mas o segundo (Alagoas)
+         deve ser inserido com unshift(), para manter a ordem alfabética. 
+         O terceiro Estado (Rio Grande do Norte) deve ser inserido entre os dois 
+         já existentes, e assim por diante.
+
+*/
